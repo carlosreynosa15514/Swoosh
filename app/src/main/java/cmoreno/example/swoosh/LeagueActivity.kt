972dbd82364c6a76245cb19037e02deac2331c33
Carlos Moreno Quintanar <carlos.sigma66@gmail.com>
2021-08-02 09:26:04 -0500

    Pantalla del League Screen usando la funcionalidad de onClick de un boton desde el XML del Layout.

--- app/src/main/java/cmoreno/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/cmoreno/example/swoosh/LeagueActivity.kt
@@ -1,8 +1,8 @@
 package cmoreno.example.swoosh
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
@@ -10,8 +10,15 @@ class LeagueActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
-        btn_next.setOnClickListener{
-                startActivity(Intent(this, PersonActivity::class.java))
-        }
+        // para no hacer este paso, se puede crear una funcion publica o sea fuera del OnCreate
+//        btn_next.setOnClickListener{
+//                startActivity(Intent(this, PersonActivity::class.java))
+//        }
+    }
+
+    // Para usar el onClick se debe poner su funcion afuera del OnCreate es decir hacerla publica.
+    fun leagueNextClick(view: View) {
+        val skillActivity = Intent(this, PersonActivity::class.java)
+        startActivity(skillActivity)
     }
 }
